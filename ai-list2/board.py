16256aa49427751a79c54b3f1c6eb352d6d4f175
Vera Yemialyanava <269565@student.pwr.edu.pl>
2024-05-08 16:29:36 +0200

    Board functions + Hamla move generation

---
+++ ai-list2/board.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+PLAYER1_STARTING_POSITIONS = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (2, 0),
+                              (2, 1), (2, 2), (2, 3), (3, 0), (3, 1), (3, 2), (4, 0), (4, 1)]
+PLAYER2_STARTING_POSITIONS = [(15, 15), (15, 14), (15, 13), (15, 12), (15, 11), (14, 15), (14, 14), (14, 13), (14, 12),
+                              (14, 11), (13, 15), (13, 14), (13, 13), (13, 12), (12, 15), (12, 14), (12, 13), (11, 15),
+                              (11, 14)]
+
+def create_game_board():
+    board = np.zeros((16, 16), dtype=int)
+
+    for x, y in PLAYER1_STARTING_POSITIONS:
+        board[x][y] = 1
+
+    for x, y in PLAYER2_STARTING_POSITIONS:
+        board[x][y] = 2
+
+    return board
+
+def check_the_winner(board):
+    player1_target = PLAYER2_STARTING_POSITIONS
+    player2_target = PLAYER1_STARTING_POSITIONS
+
+    player1_count = 0
+    player2_count = 0
+
+    for x, y in player1_target:
+        if board[x][y] == 1:
+            player1_count += 1
+
+    for x, y in player2_target:
+        if board[x][y] == 2:
+            player2_count += 1
+
+    if player1_count == 19:
+        return 1
+
+    if player2_count == 19:
+        return 2
+
+    return 0
+
+
+if __name__ == '__main__':
+    board = create_game_board()
+    print(board)
+
---
+++ ai-list2/halma.py
@@ -0,0 +1,114 @@
+import numpy as np
+
+MOVE_DIRECTIONS = [(-1, -1), (-1, 1), (1, -1), (1, 1), (1, 0), (-1, 0), (0, 1), (0, -1)]
+JUMP_DIRECTIONS = [(-2, -2), (-2, 2), (2, -2), (2, 2), (2, 0), (-2, 0), (0, 2), (0, -2)]
+
+
+class Halma:
+    def __init__(self, board, current_player):
+        self.board = board
+        self.current_player = current_player
+
+    def generate_moves(self):
+        moves = []
+        for x in range(len(self.board)):
+            for y in range(len(self.board)):
+                if self.board[x][y] == self.current_player:
+                    moves.extend(self.generate_moves_for_position(x, y))
+
+        moves = set(moves)
+        return list(moves)
+
+    def generate_moves_for_position(self, x, y, visited=None, is_recursion_call=False):
+        if visited is None:
+            visited = set()
+        visited.add((x, y))
+
+        if not is_recursion_call and self.board[x][y] != self.current_player:
+            return []
+
+        moves = []
+        for dx, dy in MOVE_DIRECTIONS:
+            new_x, new_y = x + dx, y + dy
+            jump_x, jump_y = new_x + dx, new_y + dy
+            if 0 <= new_x < len(self.board) and 0 <= new_y < len(self.board) and (new_x, new_y) not in visited:
+                if self.board[new_x][new_y] == 0:  # Simple move
+                    moves.append(Move((x, y), (new_x, new_y)))
+                elif 0 <= jump_x < len(self.board) and 0 <= jump_y < len(self.board) and (jump_x, jump_y) not in visited:
+                    if self.board[new_x][new_y] in [1, 2] and self.board[jump_x][jump_y] == 0:  # Jump
+                        moves.append(Move((x, y), (jump_x, jump_y)))
+                        moves.extend(self.generate_moves_for_position(jump_x, jump_y, visited.copy(), True))
+        return moves
+
+    def make_move(self, move):
+        self.board[move.end[0]][move.end[1]] = self.current_player
+        self.board[move.start[0]][move.start[1]] = 0
+
+    def undo_move(self, move):
+        self.board[move.start[0]][move.start[1]] = self.current_player
+        self.board[move.end[0]][move.end[1]] = 0
+
+    def __str__(self):
+        return str(self.board)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Move:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def is_legal_move(self, state):
+        start_x, start_y = self.start
+        end_x, end_y = self.end
+
+        if state.board[start_x][start_y] != state.current_player or state.board[end_x][end_y] != 0:
+            return False
+
+        if abs(end_x - start_x) <= 1 and abs(end_y - start_y) <= 1:
+            return True
+
+        return self._check_jumps(start_x, start_y, end_x, end_y, state, set())
+
+    def _check_jumps(self, current_x, current_y, end_x, end_y, state, visited):
+        if (current_x, current_y) == (end_x, end_y):
+            return True
+        visited.add((current_x, current_y))
+
+        for dir_x, dir_y in JUMP_DIRECTIONS:
+            next_x, next_y = current_x + dir_x, current_y + dir_y
+            mid_x, mid_y = current_x + dir_x // 2, current_y + dir_y // 2
+
+            print(len(state.board))
+
+            if 0 <= next_x < len(state.board) and 0 <= next_y < len(state.board) and (next_x, next_y) not in visited:
+                if state.board[next_x][next_y] == 0 and state.board[mid_x][mid_y] in [1, 2]:
+                    if self._check_jumps(next_x, next_y, end_x, end_y, state, visited):
+                        return True
+        return False
+
+    def __str__(self):
+        return f"{self.start} -> {self.end}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+if __name__ == '__main__':
+    initial_board = np.zeros((4, 4), dtype=int)
+
+    # Set up a simple scenario
+    initial_board[0][0] = 1
+    initial_board[1][1] = 1
+    initial_board[2][1] = 1
+
+    initial_board[3][3] = 2
+    initial_board[2][3] = 2
+    initial_board[3][2] = 2
+
+    print(initial_board)
+
+    game = Halma(initial_board, 1)
+    print(game.generate_moves_for_position(0, 0))
